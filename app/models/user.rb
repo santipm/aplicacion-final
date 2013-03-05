3a7538f5d09f033ee3fac95877ca439043f28628
santipm <sponce@hotmail.es>
2013-03-05 16:34:34 +0100

    Plugin devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  
+
+
+
 end
